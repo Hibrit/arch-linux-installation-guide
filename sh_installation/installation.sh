f93165eeb64415f8a56a8386785f7e5721471b77
Taşkın Nehir Doğan <taskin.n.dogan@yandex.com>
2020-10-14 21:41:08 +0300

    merge install and uefi checker

--- sh_installation/installation.sh
+++ sh_installation/installation.sh
@@ -3,6 +3,9 @@
 
 #todo use iwctl to connect the network (remember python scripts[make it binary{compile}])
 
-
-#todo 'ls /sys/firmware/efi/efivars' check if this command has an output or error if error system is mbr if not system is uefi
-UEFI = ./uefiChecker.sh "ls /sys/firmware/efi/efivars"
+#* UEFI checker
+if [ -d "/sys/firmware/efi/efivars" ]; then
+    ./uefi
+else 
+    echo "system is not uefi developers will soon release an mbr version"
+fi
--- sh_installation/uefiChecker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -d "$1" ]; then
-    ./uefi
-else 
-    echo "system is not uefi developers will soon release an mbr version"
-fi
